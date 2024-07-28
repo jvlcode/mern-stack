bdb4cc8c7d30e531c765cb673f1b240f0f55ccf1
JVLcode <81806680+jvlcode@users.noreply.github.com>
2024-07-28 17:06:50 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const postRoutes = require('./routes/posts');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/blog')
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log('DB error',err));
+
+// Use routes
+app.use('/api/posts', postRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
